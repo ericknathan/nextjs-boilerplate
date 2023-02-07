c3ac13242d54db268b7d470464bb8d895aeccb47
ericknathan <erick.capito@hotmail.com>
2023-02-07 09:53:05 -0300

    feat: add RTL config and first test

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './'
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  moduleNameMapper: {
+    '@/(.*)$': '<rootDir>/src/$1'
+  },
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+};
+
+module.exports = createJestConfig(customJestConfig);
