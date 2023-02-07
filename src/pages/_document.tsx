054f8874dcc6e0cd250017b658d283b113c63905
ericknathan <erick.capito@hotmail.com>
2023-02-07 13:40:51 -0300

    feat: setup pwa

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,9 @@ export default function Document() {
 
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
+
         <meta
           name="description"
           content="A simple project starter to work with TypeSCript, React, NextJS and Stitches"
